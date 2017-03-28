a17df3ccd34ca91169f6682033f23501f21f9519
alexzhang <524763877@qq.com>
2017-03-28 21:03:05 +0800

    finish some ejs templete

--- index.js
+++ index.js
@@ -34,6 +34,19 @@ app.use(session({
 app.use(flash());
 
 // 路由
+// 设置模板全局常量
+app.locals.blog = {
+    title: pkg.name,
+    description: pkg.description
+};
+
+// 添加模板必需的三个变量
+app.use(function (req, res, next) {
+    res.locals.user = req.session.user;
+    res.locals.success = req.flash('success').toString();
+    res.locals.error = req.flash('error').toString();
+    next();
+});
 routes(app);
 
 // 监听端口，启动程序
