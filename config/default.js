c0a3caae406853e2efd4f23151884d3ee10cdf7e
ALEX-ZHANG <524763877@qq.com>
2017-03-29 16:06:39 +0800

    11

--- config/default.js
+++ config/default.js
@@ -3,10 +3,5 @@
  */
 module.exports = {
     port: 80,
-    session: {
-        secret: 'myblog',
-        key: 'myblog',
-        maxAge: 2592000000
-    },
     mongodb: 'mongodb://myBlog:112112asd@ds145380.mlab.com:45380/blog'
 };
