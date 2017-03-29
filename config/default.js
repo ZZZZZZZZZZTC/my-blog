b94eb174f9bb339caee39c56ecc148216080a14a
ALEX-ZHANG <524763877@qq.com>
2017-03-29 15:53:43 +0800

    complete

--- config/default.js
+++ config/default.js
@@ -2,11 +2,11 @@
  * Created by 123 on 2017/2/28.
  */
 module.exports = {
-    port: 3000,
+    port: 80,
     session: {
         secret: 'myblog',
         key: 'myblog',
         maxAge: 2592000000
     },
-    mongodb: 'mongodb://localhost:27017/myblog'
+    mongodb: 'mongodb://myBlog:112112asd@ds145380.mlab.com:45380/blog'
 };
